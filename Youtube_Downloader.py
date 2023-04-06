e506f13553aa1fa5a4dfd5a3aa7e380512144e7c
Calder <email@email.com>
2023-04-06 17:07:40 -0400

    second commit

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,1 +1,21 @@
-print("Youtube Downloader App")
+import tkinter as shortcut
+
+
+root = shortcut.Tk()
+root.title("Youtube Video Downloader")
+root.geometry("500x500")
+
+searchData = shortcut.StringVar(master=root)
+
+
+def print_field():
+    print(searchData.get())
+
+
+button = shortcut.Button(root, command=print_field)
+button.pack()
+
+entry = shortcut.Entry(root, textvariable=searchData)
+entry.pack()
+
+root.mainloop()
